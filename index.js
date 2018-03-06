447525005954f109af815cf5c8c70f574976ed26
siffogh <siffogh3@gmail.com>
2018-03-06 19:02:15 +0100

    reformat code

--- index.js
+++ index.js
@@ -1,75 +1,70 @@
 const readline = require('readline');
 
+
 class Logger {
   constructor(options) {
-    this.lines = 0;
+    /**
+     * Bind member methods
+     */
+    this.proxyConsoleLog = this.proxyConsoleLog.bind(this);
     this.log = this.log.bind(this);
+    this.logLine = this.logLine.bind(this);
     this.logLineAt = this.logLineAt.bind(this);
-    this.hackConsole();
 
-    this.hackConsole = this.hackConsole.bind(this);
-    this.clear = this.clear.bind(this);
+    this.lines = 0;
+    this.proxyConsoleLog();
   }
 
-  hackConsole() {
+  // TODO: add proxy for other console methods
+  // TODO: make logLineAt support multiple lines & call it logAt
+  // TODO: make logLine support multiple lines & call it log
+  proxyConsoleLog() {
     this._log = console.log;
     console.log = this.log;
   }
 
-  clear() {
-    while (this.lines > 0) {
-      readline.clearLine(process.stdout, 0);
-      readline.moveCursor(process.stdout, 0, -1);
-      this.lines--;
-    }
-
-    readline.cursorTo(process.stdout, 0);
-  }
-
+  /**
+   * A proxy for console.log
+   */
   log(...args) {
     const result = args.reduce((res, arg) => res += arg, '');
     this.lines += result.split('\n').length;
     this._log(...args);
   }
 
-  logLine(message) {
-
+  /**
+   * Logs a single line
+   * @param line
+   * @returns {number}
+   */
+  logLine(line) {
+    this.logLineAt(this.lines, line);
+    return this.lines - 1;
   }
 
-  logLineAt(index, msg) {
+  /**
+   * Logs a single line at position index
+   * @param index
+   * @param line
+   */
+  logLineAt(index, line) {
     let n = this.lines - index;
     let stdout = process.stdout;
     readline.cursorTo(stdout, 0);
     readline.moveCursor(stdout, 0, -n);
-    stdout.write(msg);
+    stdout.write(line);
     readline.clearLine(stdout, 1);
     readline.cursorTo(stdout, 0);
 
     let newMove;
-    if(n > this.lines || n === 0) {
+    if (n > this.lines || n === 0) {
       this.lines += n + 1;
       newMove = n + 1;
     } else {
       newMove = n;
     }
-    readline.moveCursor(stdout, 0, newMove);   
+    readline.moveCursor(stdout, 0, newMove);
   }
-
-}
-
-// Pad a string with spaces on either side
-function pad(text, length, align = 'left') {
-  let pad = ' '.repeat(length - stringWidth(text));
-  if (align === 'right') {
-    return pad + text;
-  }
-
-  return text + pad;
-}
-
-// Count visible characters in a string
-function stringWidth(string) {
-  return countBreaks(stripAnsi('' + string));
 }
 
 module.exports = Logger;
