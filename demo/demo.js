2cf4a4cbcff741ef88581ffc743bcf8c76d21f49
siffogh <siffogh3@gmail.com>
2018-03-08 22:12:09 +0100

    remove wrapper demo function

--- demo/demo.js
+++ demo/demo.js
@@ -1,11 +1,7 @@
 require('../index');
 const wisdom = require('./wisdom');
 
-const demo =  () => {
-  console.log(wisdom.get());
-  setInterval(() => {
-    console.logAt(0, wisdom.get())
-  }, 500);
-};
-
-demo();
+console.log(wisdom.get());
+setInterval(() => {
+  console.logAt(0, wisdom.get())
+}, 500);
